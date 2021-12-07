169f30a4d619c56a18d01449a64da71e4faf0a6b
chinh99bg <chinhhd72@wru.vn>
2021-12-07 20:59:20 +0700

    first commit

---
+++ js/my.js
@@ -0,0 +1,6 @@
+let inputGroupSelect01 = document.getElementById("inputGroupSelect01");
+let inputGroupSelect02 = document.getElementById("inputGroupSelect02");
+let inputGroupSelect03 = document.getElementById("inputGroupSelect03");
+let btnDetermine = document.getElementById("btnDetermine");
+
+btnDetermine.addEventListener("click", hamGiDo);
